838641ab496708abdd7b8eca5eeb5a6fe19f9191
Leooo <viet_lionel@yahoo.fr>
2016-10-12 20:02:26 +0100

    first commit

---
+++ blueprints/ember-commons-2/index.js
@@ -0,0 +1,34 @@
+/*jshint node:true*/
+module.exports = {
+  description: ''
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+  fileMapTokens: function() {
+    var self = this;
+    return {
+      __root__: function(options) {
+        return '/test1'
+      }
+    };
+  },
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall: function(options) {
+  //   // Perform extra work here.
+  // }
+  //
+  // included: function(app) {
+  // this._super.included(app);
+
+  // app.import(app.bowerDirectory + '/x-button/dist/js/x-button.js');
+  // app.import(app.bowerDirectory + '/x-button/dist/css/x-button.css');
+  // }
+};
--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-commons-2'
+  name: 'ember-commons-2',
+  included: function(app, parentAddon) {
+    // var target = (parentAddon || app);
+    // Now you can modify the app / parentAddon. For example, if you wanted
+    // to include a custom preprocessor, you could add it to the target's
+    // registry:
+    //
+    //     target.registry.add('js', myPreprocessor);
+  }
 };
