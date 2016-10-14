c9b20b8dbd3fcab7c47f3a65941c9875146180aa
Leooo <viet_lionel@yahoo.fr>
2016-10-14 20:18:29 +0100

    first wokring version, copying in commons folder

--- blueprints/ember-commons-2-detail/index.js
+++ blueprints/ember-commons-2-detail/index.js
@@ -1,41 +1,45 @@
 /*jshint node:true*/
 'use strict';
 var walkSync = require('../../node_modules/walk-sync');
+var walk = require('fs-walk');
 var path = require('path');
 
+// use with: ember g ember-commons-2-detail --typeName='models' --objectName='direct-debit'
 module.exports = {
   normalizeEntityName: function(entityName) {
+    console.log('normalizeEntityName');
     // this prevents an error when the entityName is
-    // not specified (since that doesn't actually matter
-    // to us
-    let arr = entityName.split(" ");
-    this.typeName = arr[0];
-    this.objectName = arr[1];
-    console.log('entityName', this.typeName, this.objectName);
-    return entityName;
   },
   fileMapTokens: function() {
+    let typeName = this.typeName,
+    objectName = this.objectName;
     return {
       __root__: function(options) {
         return '/commons';
       },
       __typeName__: function(options) {
-        return this.typeName
+        return typeName;
       },
       __objectName__: function(options) {
-        return this.objectName
+        return objectName;
       }
     };
   },
-  filesPath: function(/* options */) {
-    return path.join(this.path, 'files').replace(
-      'blueprints/ember-commons-2/files',
-      'node_modules/ember-commons/addon'
+  filesPath: function(options) {
+    // console.log('options', options ? options : '');
+    // first run from hasPathToken
+    this.typeName = options ? options.taskOptions.typeName : '';
+    this.objectName = options ? options.taskOptions.objectName : '';
+    let filesPath = path.join(this.path, 'files').replace(
+      'blueprints/ember-commons-2-detail/files',
+      `node_modules/ember-commons/addon/${this.typeName}/${this.objectName}`
     );
+    console.log('filesPath debug', filesPath);
+    return filesPath;
   },
   files: function() {
     // https://ember-cli.com/api/files/lib_models_blueprint.js.html#l223
-    if (this._files) { return this._files; }
+    if (this._files && this.typeName.length) { return this._files; }
     var filesPath = this.filesPath(this.options);
     /*filesPath = filesPath.replace(
       'blueprints/ember-commons-2/files',
@@ -43,25 +47,58 @@ module.exports = {
     );
     console.log('fileName:');
     console.log(filesPath);*/
-    this._files = walkSync(filesPath);
-    // console.log(this._files);
+    // this._files = walkSync(filesPath, { directories: false });
+    let _files = [];
+    walk.walkSync(filesPath, function(basedir, filename, stat) {
+      _files.push(filename);
+    });
+    this._files = _files;
+    // this._files = [filesPath];
+    // console.log('filesPath', filesPath);
+    console.log('this._files', this._files);
+    // console.log('_files', this._files);
     return this._files;
   },
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
-  //
-  // included: function(app) {
-  // this._super.included(app);
-
-  // app.import(app.bowerDirectory + '/x-button/dist/js/x-button.js');
-  // app.import(app.bowerDirectory + '/x-button/dist/css/x-button.css');
-  // }
+  _process: function(options, beforeHook, process, afterHook) {
+    return this._super.apply(this, arguments);
+  },
+  locals: function() {console.log('locals');},
+  _locals: function(options) {
+    var packageName = options.project.name();
+    var moduleName = options.entity && options.entity.name || packageName;
+    var sanitizedModuleName = moduleName.replace(/\//g, '-');
+    var fileMapVariables = this._generateFileMapVariables(moduleName, undefined, options);
+    var fileMap = this.generateFileMap(fileMapVariables);
+    console.log('_process',fileMap);
+    console.log('_locals0', moduleName);
+    return this._super.apply(this,arguments).then(function(res){
+      console.log('_locals', res);
+      return res;
+    });
+  },
+  beforeInstall: function() {
+    console.log('beforeInstall');
+  },
+  buildFileInfo: function(intoDir, templateVariables, file) {
+  console.log('buildFileInfo');
+   /*let base = this._super.apply(this, arguments);
+   console.log('arguments', intoDir, templateVariables, file);
+   console.log('base', base);
+   console.log('templateVariablesdebug', templateVariables);
+   let baseName = templateVariables.dasherizedPackageName;
+   console.log('baseName', baseName);
+   let filePath = base.inputPath;
+   console.log('filePath0', filePath, filePath.indexOf('ember-commons/'));
+   filePath = filePath.substr(filePath.indexOf('ember-commons/'), filePath.length);
+   filePath = 'oam/' + filePath.replace('ember-commons/addon/', 'commons/');
+   console.log('filePath', filePath);*/
+   let baseName = templateVariables.dasherizedPackageName;
+   let base = this._super.apply(this, arguments);
+   let outputPath = base.outputPath;
+   console.log('outputPath0', outputPath);
+   outputPath = outputPath.replace(baseName, `${baseName}/commons/${this.typeName}/`);
+   console.log('outputPath', outputPath);
+   base.outputPath = outputPath;
+   return base;
+ },
 };
--- blueprints/ember-commons-2/index.js
+++ blueprints/ember-commons-2/index.js
@@ -2,14 +2,40 @@
 'use strict';
 var myModel = require('../../addon/test');
 var walkSync = require('../../node_modules/walk-sync');
+var walk = require('../../node_modules/fs-walk');
 var path = require('path');
+var Blueprint = require('ember-cli/lib/models/blueprint');
+var detailedBlueprint = require('../ember-commons-2-detail/index');
 
 //give 'unexpected reserved word' (on 'import', http://stackoverflow.com/questions/32346886/unexpected-reserved-word-import-in-node-js)
 //var myModel = require('../../node_modules/ember-commons/addon/models/premise');
 
 module.exports = {
   afterInstall: function(options, local) {
-    console.log('beforeInstall options', Object.keys(options), options);
+    console.log('path', this.path);
+    // console.log('afterInstall options', options.importFromCommons);
+    let importFromCommons = options.importFromCommons,
+    blueprintDetail = Blueprint.load(`${this.path}-detail`);
+    //blueprintDetail = new Blueprint(`${this.path}-detail/index`);
+    let test;
+    console.log('blueprintDetail', blueprintDetail);
+    let promiseArray= [];
+    Object.keys(importFromCommons).forEach((typeName) => {
+      importFromCommons[typeName].forEach((objectName) => {
+        let options2 = options;
+        options2.taskOptions = {};
+        options2.taskOptions.typeName = typeName;
+        options2.taskOptions.objectName = objectName;
+        promiseArray.push(blueprintDetail.install(options2));
+      });
+    });
+    let promise = promiseArray[0];
+    for (let i=1; i<promiseArray.length; i++) {
+      promise = promise.then(() => {
+        return promiseArray[i];
+      });
+    }
+    return promise;
   },
   normalizeEntityName: function() {
     // this prevents an error when the entityName is
@@ -38,7 +64,9 @@ module.exports = {
      );
      console.log('fileName:');
      console.log(filesPath);*/
-     this._files = walkSync(filesPath);
+     // this._files = walkSync(filesPath);
+     let _files = [];
+     this._files = _files;
      // console.log(this._files);
      return this._files;
    },
